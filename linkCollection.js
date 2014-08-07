f8d1c5474656c026dfd299bf71ff322d02d0e5b1
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-07 13:24:55 -0400

    Work on expanding full list of links

--- linkCollection.js
+++ linkCollection.js
@@ -3,4 +3,5 @@ inspireBen = ["<a href= 'https://www.youtube.com/watch?v=ApoJk9X7Vto'> Jeter Fli
               "<a href= 'https://www.youtube.com/watch?v=vUFbjCD2taw'> Brady Drive </a>",
               "<a href= 'https://www.youtube.com/watch?v=vdPQ3QxDZ1s'> Jordan Game 6 </a>",
               "<a href= 'https://www.youtube.com/watch?v=ZnvoVm6yj2A'> Pierce Game 5 </a>",
-              "<a href= 'https://www.youtube.com/watch?v=GzZWSrr5wFI'> Streets Have No Name</a>"]
+              "<a href= 'https://www.youtube.com/watch?v=GzZWSrr5wFI'> Streets Have No Name</a>",
+              "<a href= 'https://www.youtube.com/watch?v=wPm9k6ul9EI'> Kasparov Immortal Game</a>"]
