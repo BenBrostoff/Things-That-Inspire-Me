661fa0bf5b36ad43f06669cae372ade935d7e1e6
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-09 13:01:55 -0400

    Video addition

--- public/js/linkCollection.js
+++ public/js/linkCollection.js
@@ -10,4 +10,13 @@ inspireBen = ["<a href= 'https://www.youtube.com/watch?v=ApoJk9X7Vto'> Jeter Fli
               "<a href= 'https://www.youtube.com/watch?v=sxy920Nd7yY'> Lezak 100 Free </a>",
               "<a href= 'https://www.youtube.com/watch?v=JK2hKzZss5Y'> Live and Let Die </a>",
               "<a href= 'https://www.youtube.com/watch?v=DHUUyx0d7qw'> The Carousel </a>",
-              "<a href= 'https://www.youtube.com/watch?v=NBjWHfBHKos'> Good Will Hunting </a>"]
+              "<a href= 'https://www.youtube.com/watch?v=NBjWHfBHKos'> Good Will Hunting </a>",
+              "<a href= 'https://www.youtube.com/watch?v=oxzR0RKRWTQ'> Jeter Jump Throw </a>",
+              "<a href= 'https://www.youtube.com/watch?v=ndyfsS0bFK8'> Rondo Game 2 </a>",
+              "<a href= 'https://www.youtube.com/watch?v=Qc_v9mTfhC8'> Magnus Carlsen </a>",
+              "<a href= 'https://www.youtube.com/watch?v=4xdTIJb9uNA'> Manning - Manningham </a>",
+              "<a href= 'https://www.youtube.com/watch?v=JWk6_FZWIRw'> Ben Watson </a>",
+              "<a href= 'https://www.youtube.com/watch?v=w4dZ2pcLMCs'> Em - Elton </a>",
+              "<a href= 'https://www.youtube.com/watch?v=x7qPAY9JqE4'> Jobs 07 </a>",
+              "<a href= 'https://www.youtube.com/watch?v=zyjOy7fRzs0'> KG </a>",
+              "<a href= 'https://www.youtube.com/watch?v=RN1nUnCr1QM'> Pedro 99 ASG </a>"]
