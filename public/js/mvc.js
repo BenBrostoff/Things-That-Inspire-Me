c4ab8082e2f9d0af946bed433d396805b82237d4
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-29 16:38:00 -0400

    Iterate through links

--- public/js/mvc.js
+++ public/js/mvc.js
@@ -5,14 +5,14 @@
 
   app.controller('CoolLinks', function(){
     this.links = shuffle(inspireBen);
+    collect = this.links
+
     this.current = this.links[0];
     this.total = inspireBen.length;
 
 
     this.change = function(op, bound, place) {
       if (place != bound) {
-        console.log(place);
-        console.log(op(place));
         this.current = this.links[op(place)]
       }
     };
@@ -20,13 +20,13 @@
     this.add = function(){
       this.change(function(x){return x + 1}, 
                   this.total, 
-                  inspireBen.indexOf(this.current));
+                  collect.indexOf(this.current));
     }
 
     this.subtract = function(){
       this.change(function(x){return x - 1}, 
                   0, 
-                  inspireBen.indexOf(this.current));
+                  collect.indexOf(this.current));
     };
 
   });
