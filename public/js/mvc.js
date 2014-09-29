863727a599120a93683ec3e9ec097d6932abee36
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-29 16:14:38 -0400

    Refactor increment detriment into Angular

--- public/js/mvc.js
+++ public/js/mvc.js
@@ -2,9 +2,33 @@
 
   var app = angular.module('inspireMe', ["ngSanitize"]);
 
-  app.controller('CoolLinks', function(){
-    this.links = inspireBen;
-  });
 
+  app.controller('CoolLinks', function(){
+    this.links = shuffle(inspireBen);
+    this.current = this.links[0];
+    this.total = inspireBen.length;
+
+
+    this.change = function(op, bound, place) {
+      if (place != bound) {
+        console.log(place);
+        console.log(op(place));
+        this.current = this.links[op(place)]
+      }
+    };
+
+    this.add = function(){
+      this.change(function(x){return x + 1}, 
+                  this.total, 
+                  inspireBen.indexOf(this.current));
+    }
+
+    this.subtract = function(){
+      this.change(function(x){return x - 1}, 
+                  0, 
+                  inspireBen.indexOf(this.current));
+    };
+
+  });
 
 })();
